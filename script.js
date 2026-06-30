c8459608983f35e3a169e982e1bee4af4fdb6948
Prachi Bodhe <prachibodhe08@gmail.com>
2026-06-30 23:02:55 +0530

    Added portfolio

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-function showMessage() {
-    alert("Thank you for visiting my portfolio!");
+const text = [
+"Frontend Developer",
+"Web Designer",
+"JavaScript Learner",
+"GitHub Enthusiast"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let letter = "";
+
+function type(){
+
+if(i < text.length){
+
+if(j <= text[i].length){
+
+current = text[i];
+letter = current.slice(0,j++);
+
+document.getElementById("typing").textContent = letter;
+
+setTimeout(type,100);
+
 }
+
+else{
+
+j=0;
+
+i++;
+
+setTimeout(type,1000);
+
+}
+
+}
+
+else{
+
+i=0;
+
+type();
+
+}
+
+}
+
+type();
